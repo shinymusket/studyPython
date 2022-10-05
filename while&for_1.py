a5ecdf71780dc2f3a7c3d55d2c1ef2e895e56511
LAPTOP-Q8OEKGRD\grace <grace211340@gmail.com>
2022-10-05 10:48:08 +0900

    Add print_1, while&for_1

---
+++ while&for_1.py
@@ -0,0 +1,22 @@
+# while True:
+#     print('A')
+#     print('B')
+#     print('C')
+#     break
+#     print('D')
+
+# while True:
+#     print('A') # switch문 : 해당 조건 하나만 처리한다.
+#     print('B')
+#     print('C')
+#     continue
+#     print('D')
+
+#     #A, B, C만 출력되고 무한 반복출력된다.
+
+a = ["대", "한", "민", "국"]
+for i in a:
+    print(i)
+
+# 일괄 주석 처리      : ctrl + k, ctrl + c
+# 일괄 주석 처리 해제 : ctrl + k, ctrl + u
